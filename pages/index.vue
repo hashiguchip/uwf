8e3e25692e1ac78c24c293c677089d3de53c3c24
hashiguchip <hashiguchip@gmail.com>
2020-01-25 16:42:32 +0900

    🎉 first commit setup

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div>
+            <logo />
+            <h1 class="title">uwf</h1>
+            <h2 class="subtitle">uwf site</h2>
+            <div class="links">
+                <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+                    Documentation
+                </a>
+                <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">
+                    GitHub
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+// import Logo from '~/components/Logo.vue';
+//
+// export default {
+//     components: {
+//         Logo,
+//     },
+// };
+import { Vue, Component } from 'vue-property-decorator';
+
+// interface User {
+//     firstName: string;
+//     lastName: number;
+// }
+
+@Component
+export default class Index extends Vue {
+    // @Prop({ type: Object, required: true }) readonly user!: User;
+    //
+    // message: string = 'This is a message';
+    //
+    // get fullName(): string {
+    //     return `${this.user.firstName} ${this.user.lastName}`;
+    // }
+}
+</script>
+
+<style lang="sass"></style>
